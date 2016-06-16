dd8de121bc14de9c438d43f0f8643d806b5c3511
Jerome Cristante <jerome.cristante@its4u.lu>
2016-06-16 15:22:35 +0200

    Enable repository

--- src/main/java/org/rhada/persistence/repositories/MovieRepository.java
+++ src/main/java/org/rhada/persistence/repositories/MovieRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.rhada.persistences.entities.Movie;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MovieRepository extends ElasticsearchRepository<Movie, Long> {
 	
 	public List<Movie> findByName(String name);
--- src/main/java/org/rhada/rest/Application.java
+++ src/main/java/org/rhada/rest/Application.java
@@ -7,11 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableElasticsearchRepositories("org.rhada.persistence.repositories")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
